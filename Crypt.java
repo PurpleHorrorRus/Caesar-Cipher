a48a64eea7d40142b075b9980b3df35d1d84fa72
Maxim <zenit77798@yandex.ru>
2016-11-02 22:59:44 +0800

    Шифр Цезаря
    Удалил лишние строки

--- Crypt.java
+++ Crypt.java
@@ -10,27 +10,20 @@ public class Crypt extends Information {
 	void encrypt(String exp, int deg){
 		exp = exp.toUpperCase();
 		for(int i = 0, b = 0; i < exp.length(); i++, b++){
-			if(i > 0 && b < exp.length() && exp.charAt(i) == ' ') exp_encr += " "; //Пробел между словами
-			else { //Шифровка
-				if(Character.isLetter(exp.charAt(i))){
-					y = (findIndexInAlphabet(exp.charAt(i)) + deg) % alphabet.length;
-					exp_encr += alphabet[y];
-				} else exp_encr += exp.charAt(i);
-			}
-			
+			if(Character.isLetter(exp.charAt(i))){
+				y = (findIndexInAlphabet(exp.charAt(i)) + deg) % alphabet.length;
+				exp_encr += alphabet[y];
+			} else exp_encr += exp.charAt(i);	
 		}
 	}
 	
 	void decrypt(String exp, int deg){
 		exp = exp.toUpperCase();
 		for(int i = 0, b = 0; i < exp.length(); i++, b++){
-			if(i > 0 && b < exp.length() && exp.charAt(i) == ' ') exp_decr += " "; //Пробел между словами
-			else { //Дешифровка
-				if(Character.isLetter(exp.charAt(i))){
-					x = (findIndexInAlphabet(exp.charAt(i)) - deg + alphabet.length) % alphabet.length;
-					exp_decr += alphabet[x];
-				} else exp_decr += exp.charAt(i);
-			}
+			if(Character.isLetter(exp.charAt(i))){
+				x = (findIndexInAlphabet(exp.charAt(i)) - deg + alphabet.length) % alphabet.length;
+				exp_decr += alphabet[x];
+			} else exp_decr += exp.charAt(i);
 		}
 	}
 }
